e3e204971bb9e9d92a6a380e633f7613a20105f2
Sandro Abashidze <123871213+Iamyouandwhatiseeisme@users.noreply.github.com>
2026-02-16 18:47:08 +0400

    fix: remove unnecessary binary permission setting for non-Windows platforms

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,8 @@
-import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
 import * as vscode from "vscode";
 import { convertToAbstractCommand } from "./commands/convertToAbstractCommand";
 import { DartAbstractClassProvider } from "./providers/dartAbstractClassProvider";
 
 export function activate(context: vscode.ExtensionContext) {
-  if (os.platform() !== "win32") {
-    const binaryPath = path.join(context.extensionPath, "ast_extractor.exe");
-    try {
-      fs.chmodSync(binaryPath, 0o755);
-    } catch (e) {
-      console.error("Failed to set binary permissions:", e);
-    }
-  }
   // Register the command
   const disposable = vscode.commands.registerCommand(
     "dart-abstract-class-extractor.convertToAbstract",
